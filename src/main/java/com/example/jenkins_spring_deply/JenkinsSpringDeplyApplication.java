ad12931a01491ba4df258e88f85469565ad43e35
asdfkco <rlacksdhr0423@naver.com>
2024-07-02 16:51:49 +0900

    version1.0

---
+++ src/main/java/com/example/jenkins_spring_deply/JenkinsSpringDeplyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_spring_deply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringDeplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringDeplyApplication.class, args);
+	}
+
+}
